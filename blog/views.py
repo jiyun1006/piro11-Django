b265e2b11b92b9abf06cce362bc7692e977f0639
김지윤 <kimjiyoon1006@hanmail.net>
2019-07-19 14:31:02 +0900

    blog.views.post_list 뷰에 QuerySet 검색 구현

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,12 @@ from .models import Post
 
 def post_list(request):
     qs = Post.objects.all()
+
+    q = request.GET.get('q','')
+    if q:
+        qs = qs.filter(title__icontains=q)
+    #     q가 포함된 db자료만 검색해서 나타내도록
     return render(request, 'blog/post_list.html',{
         'post_list': qs,
+        'q':q
     })
